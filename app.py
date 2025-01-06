648163d5821f6cdb9a538cf8d83938a1770f77ff
Mohammed Aftab <aftabbs.wwe@gmail.com>
2025-01-06 07:10:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os 
 app = Flask(__name__)
 @app.route('/',methods=['GET'])   
-def home():    
+def home():     
     return "hello world"       
 if __name__=='__main__':    
     app.run(debug=True,host='0.0.0.0',port='5000')           
